30de69cdf0412f4849c8491c3bd3d9cbbca3eb44
kennytheeggman <kenny.kh.huang@gmail.com>
2022-01-02 17:27:26 -0500

    Update script.js

--- Y2xpcHM/script.js
+++ Y2xpcHM/script.js
@@ -1,16 +1,24 @@
-var height = window.innerHeight;
+var height = 0;
+var width = 0;
+var heightavg = 0;
+var widthavg = 0;
+var modalthresholds = [];
 var flag = true;
-var width = window.innerWidth;
 var urlname = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')+1);
 document.getElementById("modal").style.display = "none";
-var heightavg = (window.innerHeight - 562) / 2;
-var widthavg = (window.innerWidth - 1000) / 2;
-var modalthresholds = [heightavg, height - heightavg, widthavg, width - widthavg]
+setInterval(() => {
+    height = window.innerHeight;
+    width = window.innerWidth;
+    heightavg = (window.innerHeight - 562) / 2;
+    widthavg = (window.innerWidth - 1000) / 2;
+    modalthresholds = [heightavg, height - heightavg, widthavg, width - widthavg]
+}, 60);
 var elementArr = document.getElementsByClassName("clip");
 for (var i = 0; i < elementArr.length; i++) {
     var videosource = elementArr[i].childNodes[1].childNodes[1].getAttribute("src");
     console.log(elementArr[i].childNodes[1].childNodes[7].setAttribute("onclick","copy('" + videosource + "')"));   
     elementArr[i].setAttribute("onclick", "open_modal('" + videosource + "')");
+    console.log(elementArr[i].getAttribute("onclick"));
 }
 document.addEventListener("click", function(e) {
     if (document.getElementById("modal").style.display === "flex") {
@@ -27,15 +35,16 @@ function open_modal(videosrc) {
     if (flag) {
         console.log(videosrc, "open")
         document.querySelector(".player").setAttribute("src", videosrc);
-        document.getElementById("modal").style.display = "flex";
-        document.querySelector(".player").play();
+        setTimeout(() => {
+            document.getElementById("modal").style.display = "flex";
+            document.querySelector(".player").play();
+        }, 10);
     }
 }
 
 
 function copy(videosrc) {
     var urlstring = "https://kennytheeggman.github.io/" + urlname + escape(videosrc);
-    console.log(urlstring)
     navigator.clipboard.writeText(urlstring);
     set_flag();
 }
