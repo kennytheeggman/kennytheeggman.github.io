08a1b45ddf9240f58cf99eda7ee962a22b6b5da1
kennytheeggman <kenny.kh.huang@gmail.com>
2022-01-03 13:42:41 -0500

    Update script.js

--- Y2xpcHM/script.js
+++ Y2xpcHM/script.js
@@ -1,7 +1,6 @@
 // Global Variables
-var height, width, heightavg, widthavg, modalthresholds;                       // Variables for modal exit point thresholds
+var height, width, modalthresholds;                                                                 // Variables for modal exit point thresholds
 var flag = true;                                                                                    // Allow modal open flag (false when clicking copy button)
-var urlname = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')+1);   // Path to index.html URI
 // Function Declaration
 function exit_modal(){
     document.getElementById("modal").style.display = "none";                                        // Sets modal to invisible
@@ -24,18 +23,12 @@ function set_flag(val) { flag = val; }
 function add_clip(videosrc, date, clipname) {
     var datestr = date.slice(0, 4) + "/" + date.slice(4, 6) + "/" + date.slice(6);
     var content = document.getElementById("content-wrapper");                                       // Find content wrapper HTML DOM
-        var clipel = document.createElement("ul");                                                  // Create UL Element
-        content.appendChild(clipel);
-            var video = document.createElement("video");                                            // Create video element, child of UL
-            clipel.appendChild(video);
-            var p1 = document.createElement("p");                                                   // Create paragraph element, child of UL
-            clipel.appendChild(p1);                                                 
-            var date = document.createElement("p");                                                 // Create paragraph element, child of UL
-            clipel.appendChild(date);
-            var button = document.createElement("button");                                          // Create button element, child of UL
-            clipel.appendChild(button);
-                var icon = document.createElement("img");                                           // Create img element, child of button
-                button.appendChild(icon);
+        var clipel = document.createElement("ul");          content.appendChild(clipel);            // Create UL Element
+            var video = document.createElement("video");    clipel.appendChild(video);              // Create video element, child of UL
+            var p1 = document.createElement("p");           clipel.appendChild(p1);                 // Create paragraph element, child of UL
+            var date = document.createElement("p");         clipel.appendChild(date);               // Create paragraph element, child of UL
+            var button = document.createElement("button");  clipel.appendChild(button);             // Create button element, child of UL
+                var icon = document.createElement("img");   button.appendChild(icon);               // Create img element, child of button
     clipel.setAttribute("class", "clip");                                                           // Set clip methods
     clipel.setAttribute("onclick", "open_modal('" + videosrc + "')");
     p1.innerHTML = clipname;                                                                        // Set paragraph content
@@ -56,13 +49,12 @@ for (var i = clips.length - 1; i >= 0; i--) {
 // Continuous Scripts
 setInterval(() => {                                                                                 // Recalculate modal thresholds in case of window resize
     height = window.innerHeight, width = window.innerWidth;
-    heightavg = (window.innerHeight - 562) / 2, widthavg = (window.innerWidth - 1000) / 2;
-    modalthresholds = [heightavg, height - heightavg, widthavg, width - widthavg]
+    modalthresholds = [width / 2 - 500, width / 2 + 500, height / 2 - 281, height / 2 + 281];
 }, 60);
-document.addEventListener("click", function(e) {                                                    //  When modal is active
+document.addEventListener("click", function(e) {                                                    // When modal is active
     if ((document.getElementById("modal").style.display === "flex") && (                            // If click is outside modal_window
-        e.clientY < modalthresholds[0] || e.clientY > modalthresholds[1] ||
-        e.clientX < modalthresholds[2] || e.clientX > modalthresholds[3])) {
+        e.clientX < modalthresholds[0] - 10 || e.clientX > modalthresholds[1] + 10 ||               // Margin buffer increase
+        e.clientY < modalthresholds[2] - 10 || e.clientY > modalthresholds[3] + 10 )) {
             exit_modal();                                                                           // Exit modal
     }
 });
